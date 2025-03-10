a92fd56ab9939bfd79f97e4047eea5cf782e22eb
Hiển Lê <hienlq3@fpt.com>
2025-03-10 08:27:52 +0700

    run android device

--- packages/core_package/lib/injection.config.dart
+++ packages/core_package/lib/injection.config.dart
@@ -11,6 +11,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:core_package/register_module.dart' as _i342;
 import 'package:core_package/src/service/base_service.dart' as _i446;
+import 'package:core_package/src/service/my_approval_service.dart' as _i201;
 import 'package:core_package/src/service/system_property_service.dart' as _i245;
 import 'package:core_package/src/service/user_service.dart' as _i709;
 import 'package:dio/dio.dart' as _i361;
@@ -32,6 +33,8 @@ extension GetItInjectableX on _i174.GetIt {
     gh.lazySingleton<_i361.Dio>(() => dioModule.dio());
     gh.singleton<_i446.BaseService>(
         () => _i446.BaseService(dio: gh<_i361.Dio>()));
+    gh.singleton<_i201.MyApprovalService>(
+        () => _i201.MyApprovalService(baseService: gh<_i446.BaseService>()));
     gh.singleton<_i245.SystemPropertyService>(() =>
         _i245.SystemPropertyService(baseService: gh<_i446.BaseService>()));
     gh.singleton<_i709.UserService>(
